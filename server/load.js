1d56d721b541965c75fbfa076f8b4d49cd80d77c
Brent Hoover <brent@thebuddhalodge.com>
2016-07-11 11:21:28 +0800

    Add Shipping fixture

--- server/load.js
+++ server/load.js
@@ -22,18 +22,20 @@ function modifyCheckoutWorkflow() {
  * Hook to setup core additional imports during ReactionCore init (shops process first)
  */
 if (Hooks) {
-  Hooks.Events.add("afterCoreInit", () => {
-    Logger.info("Initialize using Bees Knees Data");
-    Reaction.Import.fixture().process(Assets.getText("data/Shops.json"), ["name"], Reaction.Import.shop);
+  Hooks.Events.add("onCoreInit", () => {
+    Logger.info("======> Initialize using Bees Knees Data");
+    Reaction.Import.fixture().process(Assets.getText("private/data/Shops.json"), ["name"], Reaction.Import.shop);
     // ensure Shops are loaded first.
     Reaction.Import.flush(Shops);
     // these will flush/import with the rest of the imports from core init.
-    Reaction.Import.fixture().process(Assets.getText("data/Products.json"), ["title"], Reaction.Import.load);
-    Reaction.Import.fixture().process(Assets.getText("data/Tags.json"), ["name"], Reaction.Import.load);
+    Reaction.Import.fixture().process(Assets.getText("private/data/Products.json"), ["title"], Reaction.Import.load);
+    Reaction.Import.fixture().process(Assets.getText("private/data/Tags.json"), ["name"], Reaction.Import.load);
+    Reaction.Import.fixture().process(Assets.getText("private/data/Shipping.json"), ["name"], Reaction.Import.load);
     Reaction.Import.flush();
   });
 
   Hooks.Events.add("afterCoreInit", () => {
+    Logger.info("Modifying checkout workflow");
     modifyCheckoutWorkflow();
   });
 }
