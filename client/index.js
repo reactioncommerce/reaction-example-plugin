5761c1c691e26ce7aeb670411ac76ae36c1673b7
Brent Hoover <brent@thebuddhalodge.com>
2016-07-26 14:44:51 +0800

    Eliminate stuff that overlapped with theming, Also don't add templates that we aren't doing anything significant with.

--- client/index.js
+++ client/index.js
@@ -1,4 +1,2 @@
-import "./styles/main.less";
 import "./templates";
-import "bootstrap/dist/js/npm.js";
 import "./defaults";
--- client/templates/index.js
+++ client/templates/index.js
@@ -4,24 +4,9 @@ import "./checkout/review/review";
 
 // layouts
 import "./layouts/core.html";
-import "./layouts/footer.html";
-import "./layouts/header/header";
-import "./layouts/header/brand.html";
-import "./layouts/header/button.html";
-import "./layouts/header/button.html";
-import "./layouts/header/navbar.html";
-import "./layouts/header/tags.html";
-import "./layouts/header/navbar.html";
+
 
 // products
-import "./products/productGrid/content.html";
-import "./products/productGrid/content.js";
-import "./products/productGrid/controls.html";
-import "./products/productGrid/controls.js";
-import "./products/productGrid/item.html";
-import "./products/productGrid/notice.html";
-import "./products/productGrid/productGrid.html";
-import "./products/productGrid/productGrid.js";
 import "./products/productsLanding.html";
 import "./products/productsLanding";
 
