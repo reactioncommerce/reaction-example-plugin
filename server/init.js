917ec4500131ec7814b0464857e94ca8f9975224
Brent Hoover <brent@thebuddhalodge.com>
2016-08-03 10:30:25 +0800

    Rewrite layout records to use custom layout

--- server/init.js
+++ server/init.js
@@ -1,18 +1,19 @@
-import { Packages, Shops } from "/lib/collections";
-import { Hooks, Reaction, Logger } from "/server/api";
+import {Packages, Shops} from "/lib/collections";
+import {Hooks, Reaction, Logger} from "/server/api";
+import {check} from "meteor/check";
 
 function modifyCheckoutWorkflow() {
   // Replace checkoutReview with our custom Template
   Logger.info("::: Modifying checkout workflow");
   Packages.update({
-    "name": "reaction-checkout",
-    "layout": {
-      "$elemMatch": {
-        "template": "checkoutReview"
+    name: "reaction-checkout",
+    layout: {
+      $elemMatch: {
+        template: "checkoutReview"
       }
     }
   }, {
-    "$set": {
+    $set: {
       "layout.$.template": "checkoutReviewBeesknees",
       "layout.$.label": "Review Order"
     }
@@ -24,11 +25,23 @@ function addRolesToVisitors() {
   Logger.info("::: Adding about route permissions to default roles");
   const shop = Shops.findOne(Reaction.getShopId());
   Shops.update(shop._id, {
-      $addToSet: { "defaultVisitorRole": "about"}
-    }
-  );
+    $addToSet: { defaultVisitorRole: "about" }
+  });
   Shops.update(shop._id, {
-    $addToSet: { "defaultRole": "about"}
+    $addToSet: { defaultRole: "about" }
+  });
+}
+
+function changeLayouts(shopId, newLayout) {
+  check(shopId, String);
+  check(newLayout, String);
+  Logger.info(`::: changing all layouts to ${newLayout}`);
+  let shop = Shops.findOne(shopId);
+  for (let i = 0; i < shop.layout.length; i++) {
+    shop.layout[i].layout = newLayout;
+  }
+  return Shops.update(shopId, {
+    $set: {layout: shop.layout}
   });
 }
 
@@ -38,4 +51,5 @@ function addRolesToVisitors() {
 Hooks.Events.add("afterCoreInit", () => {
   modifyCheckoutWorkflow();
   addRolesToVisitors();
+  changeLayouts(Reaction.getShopId(), "coreLayoutBeesknees");
 });
