a7cfc097cdfffd71f20d4e55e1a50c82cf73f248
MichaelJenny <michael.jenny@projekt-und-partner.de>
2017-11-15 15:58:36 +0100

    Fix: Error when adding to cart, because  googlemap script was included multiple times.

--- client/components/availabilityMap.js
+++ client/components/availabilityMap.js
@@ -1,37 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { DocHead } from "meteor/kadira:dochead";
+import { $ } from "meteor/jquery";
 import { i18next } from "/client/api";
+
 
 class AvailabilityMap extends React.Component {
   static propTypes = {
     trackingId: PropTypes.string.isRequired
   };
 
+  renderMap() {
+    // eslint-disable-next-line no-undef, no-new
+    const googleMap = new google.maps.Map(this.refs.map, {
+      center: {
+        lat: this.props.product.lat,
+        lng: this.props.product.lng
+      },
+      zoom: 13
+    });
+
+    // eslint-disable-next-line no-undef, no-new
+    new google.maps.Marker({
+      position: {
+        lat: this.props.product.lat,
+        lng: this.props.product.lng
+      },
+      map: googleMap,
+      title: i18next.t("buyHere", "Buy here!")
+    });
+  }
+
   componentDidMount() {
     if (this.props.trackingId) {
-      const url = `https://maps.googleapis.com/maps/api/js?key=${this.props.trackingId}`;
-
-      DocHead.loadScript(url, () => {
-        // eslint-disable-next-line no-undef, no-new
-        const googleMap = new google.maps.Map(this.refs.map, {
-          center: {
-            lat: this.props.product.lat,
-            lng: this.props.product.lng
-          },
-          zoom: 13
+      if ($("#mapsHead").length === 0) {
+        const url = `https://maps.googleapis.com/maps/api/js?key=${this.props.trackingId}`;
+        DocHead.loadScript(url, { attrs: { id: "mapsHead" } }, () => {
+          this.renderMap();
         });
-
-        // eslint-disable-next-line no-undef, no-new
-        new google.maps.Marker({
-          position: {
-            lat: this.props.product.lat,
-            lng: this.props.product.lng
-          },
-          map: googleMap,
-          title: i18next.t("buyHere", "Buy here!")
-        });
-      });
+      } else {
+        this.renderMap();
+      }
     }
   }
 
@@ -39,7 +48,7 @@ class AvailabilityMap extends React.Component {
     return (
       <div>
         <h3>{i18next.t("availableLocations", "Available at the following stores")}</h3>
-        <div className="map" ref="map"></div>
+        <div className="map" ref="map" />
       </div>
     );
   }
